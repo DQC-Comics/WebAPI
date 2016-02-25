8bf05af65d6b519f3f0ed268bc0dc7dbbc1e6b08
Jonas Karlman <jonas@kwiboo.se>
2016-02-25 18:22:09 +0100

    Initialize database

--- WebAPI/Global.asax.cs
+++ WebAPI/Global.asax.cs
@@ -1,11 +1,16 @@
 ﻿namespace DQC.Comics.WebAPI
 {
+    using System.Data.Entity;
+    using System.Web;
     using System.Web.Http;
+    using DQC.Comics.WebAPI.Migrations;
+    using DQC.Comics.WebAPI.Models;
 
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ComicsDbContext, Configuration>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
